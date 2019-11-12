881cd89f8fee7b2e65baa28d5a648c672724e894
William Felicio Freire <william.freire@credisis.local>
2019-11-12 16:08:29 -0300

    updating

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,28 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-10">
-            <div class="card">
-                <painel titulo="Dashboard">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <caixa quantidade="80" titulo="Gráficos" url="#" cor="orange" icone="ion ion-pie-graph"></caixa>
-                        </div>
-                        <div class="col-md-4">
-                        <caixa quantidade="60" titulo="Artigos" url="google.com" cor="green" icone="ion ion-quote"></caixa>
-                        </div>
-                        <div class="col-md-4">
-                        <caixa quantidade="36" titulo="TCC's" url="#" cor="red" icone="ion ion-person-add"></caixa>
-                        </div>
-
-                    </div>
-
-
-
-                </painel>
+<pagina tamanho="10">
+    <painel titulo="Dashboard">
+        <div class="row">
+            <div class="col-md-4">
+                <caixa quantidade="80" titulo="Artigos" url="#" cor="#dc3545" icone="ion ion-pie-graph"></caixa>
+            </div>
+            <div class="col-md-4">
+                <caixa quantidade="60%" titulo="Graficos" url="#" cor="#ffc107" icone="ion ion-quote"></caixa>
+            </div>
+            <div class="col-md-4">
+                <caixa quantidade="36" titulo="TCC's" url="#" cor="#17a2b8" icone="ion ion-person-add"></caixa>
             </div>
         </div>
-    </div>
-    @endsection
+    </painel>
+</pagina>
+@endsection
